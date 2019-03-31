6089d62bd76b794583deec9414fa53564a0e6baf
Ay Has <ahashim983@yahoo.com>
2019-03-31 19:36:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,14 @@ def monopoly
 end
 
 def monopoly_with_second_tier
+  
   monopoly = {
     railroads: {
-      pieces: 4
+      pieces: 4, 
+      names: {}, 
+      rent_in_dollars: {}
     }
   }
 end
+
+
